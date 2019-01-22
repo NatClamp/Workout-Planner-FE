4319bac06a0471e7a5db06bfcb538b0e4a4f913f
Jake <heaney_jake@outlook.com>
2019-01-22 12:15:00 +0000

    working menu nav button

--- workout-planner-fe/navigation/Navigators.js
+++ workout-planner-fe/navigation/Navigators.js
@@ -1,15 +1,34 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
 import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
 import HomePage from '../views/HomePage';
 import WorkoutPreview from '../views/WorkoutPreview';
 import CompanionScreen from '../views/CompanionScreen';
 import UserProfile from '../views/UserProfile';
+import { Icon } from 'native-base';
+
+const DrawerIcon = ({ navigation }) => {
+	return (
+		<TouchableOpacity
+			style={{ padding: 5, paddingLeft: 10, paddingRight: 15 }}
+			onPress={() => {
+				navigation.toggleDrawer();
+			}}
+		>
+			<Text style={{ color: 'white', fontSize: 25 }}>
+				<Icon name='menu' />
+			</Text>
+		</TouchableOpacity>
+	);
+};
 
 const Stack = {
 	Home: {
 		screen: HomePage,
-		navigationOptions: {
-			title: 'Home'
-		}
+		navigationOptions: ({ navigation }) => ({
+			title: 'Home',
+			headerLeft: <DrawerIcon navigation={navigation} />
+		})
 	},
 	WorkoutPreview: {
 		screen: WorkoutPreview,
