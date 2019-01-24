9ae4cfdb4877c9fbe030cc80887b54eca2773e9d
NatClamp <nat.clamp@gmail.com>
2019-01-24 12:24:48 +0000

    making test changes to app navigation to fit in authentification nav

--- workout-planner-fe/App.js
+++ workout-planner-fe/App.js
@@ -1,29 +1,74 @@
 import React from 'react';
-import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator,
+} from 'react-navigation';
 // import RootNavigator from './navigation/Navigators';
+import WorkoutPreview from '../views/WorkoutPreview';
+import CompanionScreen from '../views/CompanionScreen';
+import UserProfile from '../views/UserProfile';
 import Loading from './views/Loading';
 import HomePage from './views/HomePage';
 import SignIn from './views/SignIn';
 
-// const AppContainer = createAppContainer(RootNavigator);
+const WorkoutStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomePage,
+      navigationOptions: {
+        title: 'Home',
+      },
+    },
+    WorkoutPreview: {
+      screen: WorkoutPreview,
+      navigationOptions: {
+        title: 'Workout Preview',
+      },
+    },
+    CompanionScreen: {
+      screen: CompanionScreen,
+      navigationOptions: {
+        title: 'Workout Companion',
+      },
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
 
-// export default class App extends React.Component {
-// 	render() {
-// 		return (
-// 			<AppContainer onUserLogout={() => this.setState({ isLoggedIn: false })} />
-//     )
-// 	}
-// }
+// const AppTabs = createBottomTabNavigator({
+//   Home: {
+//     screen: HomePage,
+//     title: 'Home',
+//   },
+//   Profile: {
+//     screen: 'UserProfile',
+//   },
+// });
+
+const AuthStack = createStackNavigator({
+  HomePage: {
+    screen: WorkoutStack,
+  },
+  SignIn: {
+    screen: 'SignIn',
+    navigationOptions: {
+      title: 'Sign In',
+    },
+  },
+});
 
 const AppContainer = createAppContainer(
   createSwitchNavigator(
     {
-      Loading,
-      HomePage,
-      SignIn,
+      AuthentificationCheck: Loading,
+      Auth: AuthStack,
     },
     {
-      initialRouteName: 'Loading',
+      initialRouteName: 'AuthentificationCheck',
     },
   ),
 );
