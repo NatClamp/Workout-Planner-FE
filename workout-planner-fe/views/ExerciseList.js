ceeeacd09c14f4d1303c54f049ae49ef67b004f1
Jake <heaney_jake@outlook.com>
2019-01-28 10:53:06 +0000

    added search bar functionality - need to decide how list is going to be displayed

--- workout-planner-fe/views/ExerciseList.js
+++ workout-planner-fe/views/ExerciseList.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Accordion, Container, Item, Icon, Input, Button } from 'native-base';
+import { SearchBar } from 'react-native-elements';
 
 const URL = 'https://nc-project-be.herokuapp.com/api/';
 
@@ -8,44 +9,39 @@ class ExerciseList extends Component {
 	state = {
 		exercises: []
 	};
+
+	updateSearch = (search) => {
+		this.setState({ search });
+	};
+
 	render() {
 		return (
 			<Fragment>
 				<View style={{ flex: 1 }}>
-					<Container
-						style={{
-							flex: 1,
-							flexDirection: 'row',
-							justifyContent: 'center'
-						}}
-						searchBar
-						rounded
-					>
-						<Item
-							style={{
-								flex: 1,
-								flexDirection: 'row',
-								justifyContent: 'center'
-							}}
-						>
-							<Icon name='ios-search' />
-							<Input placeholder='Search...' />
-						</Item>
-						<Button
-							style={{
-								paddingRight: 10,
-								paddingTop: 10
-							}}
-							transparent
-						>
-							<Text>Search</Text>
-						</Button>
-					</Container>
+					<SearchBar
+						placeholder='Type Here...'
+						onChangeText={(text) => this.searchFilterFunction(text)}
+						autoCorrect={false}
+					/>
+					{/* <FlatList
+						data={this.state.exercises}
+						renderItem={({ item }) => <Text>{item.title}</Text>}
+						keyExtractor={({ title }) => title}
+					/> */}
 					<Accordion data={this.state.exercises} />
 				</View>
 			</Fragment>
 		);
 	}
+
+	searchFilterFunction = (text) => {
+		const newData = this.state.exercises.filter((exercise) => {
+			const exerciseData = `${exercise.title.toUpperCase()}`;
+			const textData = text.toUpperCase();
+			return exerciseData.indexOf(textData) > -1;
+		});
+		this.setState({ exercises: newData });
+	};
 
 	componentDidMount() {
 		return fetch(`${URL}/exercises`)
