c98633d7260daaf22d5a018b94e203d74d82594b
mike-newh <zq@live.co.uk>
2019-01-28 15:35:25 +0000

    ability to convert fb user to app account

--- workout-planner-fe/utils/backendAPI.js
+++ workout-planner-fe/utils/backendAPI.js
@@ -7,4 +7,5 @@ module.exports = {
   getCompletedWorkouts: username => fetch(`${URL}users/${username}/completed_workouts`).then(res => res.json()).then(res => res.userCompleted).catch((err) => { console.log(err); }),
   getSavedWorkouts: username => fetch(`${URL}users/${username}/saved_workouts`).then(res => res.json()).then(res => res.userSaved).catch((err) => { console.log(err); }),
   patchUserGender: (username, gender) => Axios.patch(`${URL}users/${username}/`, { newName: username, isFemale: gender }),
+  getAllUsers: () => fetch(`${URL}users/`).then(res => res.json()),
 };
--- workout-planner-fe/views/HomePage.js
+++ workout-planner-fe/views/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Button, AsyncStorage } from 'react-native';
 import Model from '../components/Model';
 import { Accordion, Container, Content, Card, CardItem, Text } from 'native-base';
+import {getAllUsers} from '../utils/backendAPI'
 
 const URL = 'https://nc-project-be.herokuapp.com/api/';
 
@@ -9,15 +10,37 @@ export default class HomeScreen extends React.Component {
 
   state = {
     currentUser: '',
-    workout: [],
+	workout: [],
+	muscleVals:   {abdominals: 0, biceps: 0, calves: 0, chest: 0, forearms: 0, glutes: 0, hamstrings: 0, lowerback: 0, midback: 0, quadriceps: 0, shoulders: 0, obliques: 0, triceps: 0, upperback: 0},
+	appUserAccount: {},
   };
+
+
+
+  componentDidUpdate(prevProps, prevState){
+	if (prevState.workout !== this.state.workout){
+		this.calculateMuscleVals()
+	}
+	
+}
+calculateMuscleVals = () => {
+	const {workout} = this.state;
+	const muscleVals =   {abdominals: 0, biceps: 0, calves: 0, chest: 0, forearms: 0, glutes: 0, hamstrings: 0, lowerback: 0, midback: 0, quadriceps: 0, shoulders: 0, obliques: 0, triceps: 0, upperback: 0}
+	workout.forEach((exercise)=>{
+		muscleVals[exercise.major_muscle] += 3;
+		exercise.minor_muscles.forEach((muscle)=>{
+			muscleVals[muscle] +=  1
+		})
+	})
+	this.setState({muscleVals})
+}
   render() {
     // const currentUser = this.props.navigation.getParam('currentUser');
     console.log(' from the state on homepage ====>', this.state.currentUser);
     return (
 			<View style={{ flex: 1 }}>
 				<View style={{ height: 350, marginTop: 10 }}>
-					<Model />
+					<Model muscleVals={this.state.muscleVals}/>
 				</View>
 				<View>
 					<Button title='View All Exercises' onPress={() => this.props.navigation.navigate('ExerciseList')} />
@@ -76,16 +99,29 @@ export default class HomeScreen extends React.Component {
       </View>
     );
   }
+  convertFBLogin = (allUsers) =>{
+	const currentUser = allUsers.users.filter((user)=>{
+		const fbUser = this.state.currentUser.slice(1, this.state.currentUser.length-1)
+		return user.actual_name === fbUser;
+	})
+	if (currentUser.length > 1) {console.log('NON UNIQUE LOGIN CREDENTIALS')}
+	else {this.setState({appUserAccount: currentUser})}
+  }
 
   componentDidMount() {
-    this.getCurrentUser();
+	this.getCurrentUser();
 	}
-	
+
   getCurrentUser = async () => {
     try {
       const currentUser = await AsyncStorage.getItem('currentUser');
       if (currentUser) {
-        this.setState({ currentUser });
+		this.setState({ currentUser }, ()=>{
+			getAllUsers().then((data)=>{
+				this.convertFBLogin(data)
+			});
+		});
+	
       } else {
         console.log('error in the getCurrentUser function');
       }
