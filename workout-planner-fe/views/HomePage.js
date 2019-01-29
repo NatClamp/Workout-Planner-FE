4c7d0e421a87a4c1bd89ad8eebc5134d52ee0ca5
NatClamp <nat.clamp@gmail.com>
2019-01-29 10:16:15 +0000

    colouring model as exercises are completed on companion page

--- workout-planner-fe/views/HomePage.js
+++ workout-planner-fe/views/HomePage.js
@@ -7,187 +7,195 @@ import { getAllUsers } from '../utils/backendAPI';
 const URL = 'https://nc-project-be.herokuapp.com/api/';
 
 export default class HomeScreen extends React.Component {
-	state = {
-		currentUser: '',
-		workout: [],
-		muscleVals: {
-			abdominals: 0,
-			biceps: 0,
-			calves: 0,
-			chest: 0,
-			forearms: 0,
-			glutes: 0,
-			hamstrings: 0,
-			lowerback: 0,
-			midback: 0,
-			quadriceps: 0,
-			shoulders: 0,
-			obliques: 0,
-			triceps: 0,
-			upperback: 0
-		},
-		appUserAccount: {}
-	};
+  state = {
+    currentUser: '',
+    workout: [],
+    muscleVals: {
+      abdominals: 0,
+      biceps: 0,
+      calves: 0,
+      chest: 0,
+      forearms: 0,
+      glutes: 0,
+      hamstrings: 0,
+      lowerback: 0,
+      midback: 0,
+      quadriceps: 0,
+      shoulders: 0,
+      obliques: 0,
+      triceps: 0,
+      upperback: 0,
+    },
+    appUserAccount: {},
+  };
 
-	componentDidUpdate(prevProps, prevState) {
-		if (prevState.workout !== this.state.workout) {
-			this.calculateMuscleVals();
-		}
-		if (prevState.appUserAccount !== this.setState.appUserAccount) {
-			this.setUserAccount();
-		}
-	}
-	setUserAccount = () => {
-		AsyncStorage.setItem('userAccount', JSON.stringify(this.state.appUserAccount));
-		console.log('stored!');
-	};
-	calculateMuscleVals = () => {
-		const { workout } = this.state;
-		const muscleVals = {
-			abdominals: 0,
-			biceps: 0,
-			calves: 0,
-			chest: 0,
-			forearms: 0,
-			glutes: 0,
-			hamstrings: 0,
-			lowerback: 0,
-			midback: 0,
-			quadriceps: 0,
-			shoulders: 0,
-			obliques: 0,
-			triceps: 0,
-			upperback: 0
-		};
-		workout.forEach((exercise) => {
-			muscleVals[exercise.major_muscle] += 3;
-			exercise.minor_muscles.forEach((muscle) => {
-				muscleVals[muscle] += 1;
-			});
-		});
-		this.setState({ muscleVals });
-	};
-	render() {
-		// const currentUser = this.props.navigation.getParam('currentUser');
-		console.log(' from the state on homepage ====>', this.state.currentUser);
-		return (
-			<View style={{ flex: 1 }}>
-				<View style={{ height: 350, marginTop: 10 }}>
-					<Model muscleVals={this.state.muscleVals} />
-				</View>
-				<View style={styles.container}>
-					<View style={styles.buttonContainer}>
-						<Button title='All Exercises' onPress={() => this.props.navigation.navigate('ExerciseList')} />
-					</View>
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.workout !== this.state.workout) {
+      this.calculateMuscleVals();
+    }
+    if (prevState.appUserAccount !== this.setState.appUserAccount) {
+      this.setUserAccount();
+    }
+  }
+  setUserAccount = () => {
+    AsyncStorage.setItem('userAccount', JSON.stringify(this.state.appUserAccount));
+    console.log('stored!');
+  };
+  calculateMuscleVals = () => {
+    const { workout } = this.state;
+    const muscleVals = {
+      abdominals: 0,
+      biceps: 0,
+      calves: 0,
+      chest: 0,
+      forearms: 0,
+      glutes: 0,
+      hamstrings: 0,
+      lowerback: 0,
+      midback: 0,
+      quadriceps: 0,
+      shoulders: 0,
+      obliques: 0,
+      triceps: 0,
+      upperback: 0,
+    };
+    workout.forEach(exercise => {
+      muscleVals[exercise.major_muscle] += 3;
+      exercise.minor_muscles.forEach(muscle => {
+        muscleVals[muscle] += 1;
+      });
+    });
+    this.setState({ muscleVals });
+  };
+  render() {
+    console.log(' from the state on homepage ====>', this.state.currentUser);
+    const { workout } = this.state;
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ height: 350, marginTop: 10 }}>
+          <Model muscleVals={this.state.muscleVals} />
+        </View>
+        <View style={styles.container}>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="All Exercises"
+              onPress={() => this.props.navigation.navigate('ExerciseList')}
+            />
+          </View>
 
-					<View style={styles.buttonContainer}>
-						<Button
-							title='Add Exercise'
-							onPress={() => {
-								this.props.navigation.navigate('MuscleScreen', {
-									addExerciseToWorkout: this.addExerciseToWorkout
-								});
-							}}
-						/>
-					</View>
-					<View style={styles.buttonContainer}>
-						<Button
-							title='Create'
-							onPress={() => {
-								this.props.navigation.navigate('CreateExerciseForm');
-							}}
-						/>
-					</View>
-				</View>
-				<View style={{ flex: 1 }}>
-					<Content>
-						{this.state.workout.length === 0 ? (
-							<Card>
-								<CardItem>
-									<Text>Please add an exercise to your workout</Text>
-								</CardItem>
-							</Card>
-						) : (
-							this.state.workout.map((item, index) => {
-								return (
-									<Card key={index}>
-										<CardItem header>
-											<Text>{item.title}</Text>
-										</CardItem>
-									</Card>
-								);
-							})
-						)}
-					</Content>
-				</View>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Add Exercise"
+              onPress={() => {
+                this.props.navigation.navigate('MuscleScreen', {
+                  addExerciseToWorkout: this.addExerciseToWorkout,
+                });
+              }}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Create"
+              onPress={() => {
+                this.props.navigation.navigate('CreateExerciseForm');
+              }}
+            />
+          </View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <Content>
+            {this.state.workout.length === 0 ? (
+              <Card>
+                <CardItem>
+                  <Text>Please add an exercise to your workout</Text>
+                </CardItem>
+              </Card>
+            ) : (
+              this.state.workout.map((item, index) => {
+                return (
+                  <Card key={index}>
+                    <CardItem header>
+                      <Text>{item.title}</Text>
+                    </CardItem>
+                  </Card>
+                );
+              })
+            )}
+          </Content>
+        </View>
 
-				<View>
-					<Button
-						style={{ flex: 1, marginTop: 10, backgroundColor: 'blue' }}
-						title='Workout Preview'
-						onPress={() =>
-							this.props.navigation.navigate('WorkoutPreview', {
-								currentWorkout: this.state.workout,
-								currentUser: this.state.currentUser,
-								appUserAccount: this.state.appUserAccount
-							})}
-					/>
-				</View>
-			</View>
-		);
-	}
-	convertFBLogin = (allUsers) => {
-		const currentUser = allUsers.users.filter((user) => {
-			const fbUser = this.state.currentUser.slice(1, this.state.currentUser.length - 1);
-			return user.actual_name === fbUser;
-		});
-		if (currentUser.length > 1) {
-			console.log('NON UNIQUE LOGIN CREDENTIALS');
-		} else {
-			this.setState({ appUserAccount: currentUser[0] });
-		}
-	};
+        <View>
+          {workout.length > 0 && (
+            <Button
+              style={{ flex: 1, marginTop: 10, backgroundColor: 'blue' }}
+              title="Workout Preview"
+              onPress={() =>
+                this.props.navigation.navigate('WorkoutPreview', {
+                  currentWorkout: this.state.workout,
+                  currentUser: this.state.currentUser,
+                  appUserAccount: this.state.appUserAccount,
+                })
+              }
+            />
+          )}
+        </View>
+      </View>
+    );
+  }
+  convertFBLogin = allUsers => {
+    const currentUser = allUsers.users.filter(user => {
+      const fbUser = this.state.currentUser.slice(1, this.state.currentUser.length - 1);
+      return user.actual_name === fbUser;
+    });
+    if (currentUser.length > 1) {
+      console.log('NON UNIQUE LOGIN CREDENTIALS');
+    } else {
+      this.setState({ appUserAccount: currentUser[0] });
+    }
+  };
 
-	componentDidMount() {
-		this.getCurrentUser();
-	}
+  componentDidMount() {
+    this.getCurrentUser();
+  }
 
-	getCurrentUser = async () => {
-		try {
-			const currentUser = await AsyncStorage.getItem('currentUser');
-			if (currentUser) {
-				this.setState({ currentUser }, () => {
-					getAllUsers().then((data) => {
-						this.convertFBLogin(data);
-					});
-				});
-			} else {
-				console.log('error in the getCurrentUser function');
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  getCurrentUser = async () => {
+    try {
+      const currentUser = await AsyncStorage.getItem('currentUser');
+      if (currentUser) {
+        this.setState({ currentUser }, () => {
+          getAllUsers().then(data => {
+            this.convertFBLogin(data);
+          });
+        });
+      } else {
+        console.log('error in the getCurrentUser function');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	addExerciseToWorkout = (exerciseName) => {
-		const formattedExercise =
-			exerciseName.split(' ').length > 1 ? exerciseName.split(' ').join('%20') : exerciseName;
-		return fetch(`${URL}/exercises/${formattedExercise}`).then((response) => response.json()).then((respJSON) => {
-			const workout = [ ...this.state.workout ];
-			workout.push(respJSON.exercise);
-			this.setState({ workout });
-		});
-	};
+  addExerciseToWorkout = exerciseName => {
+    const formattedExercise =
+      exerciseName.split(' ').length > 1 ? exerciseName.split(' ').join('%20') : exerciseName;
+    return fetch(`${URL}/exercises/${formattedExercise}`)
+      .then(response => response.json())
+      .then(respJSON => {
+        const workout = [...this.state.workout];
+        workout.push(respJSON.exercise);
+        this.setState({ workout });
+      });
+  };
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	buttonContainer: {
-		flex: 1
-	}
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
 });
