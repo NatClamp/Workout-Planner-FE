9f9345d11a5ac82704155812e86f57b09831d3a7
Jake <heaney_jake@outlook.com>
2019-01-30 15:56:48 +0000

    function to send workout to completed workouts

--- workout-planner-fe/views/CompanionScreen.js
+++ workout-planner-fe/views/CompanionScreen.js
@@ -1,124 +1,169 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import Model from '../components/Model';
-import { Container, Header, Content, Accordion } from 'native-base';
+import { Container, Content } from 'native-base';
 import { ListItem, CheckBox } from 'react-native-elements';
-import CompletionModal from './CompletionModal';
+import axios from 'axios';
 
 const URL = 'https://nc-project-be.herokuapp.com/api/';
 
 export default class CompanionScreen extends React.Component {
-  state = {
-    exercises: [],
-    checked: [],
-    muscleVals: {
-      abdominals: 0,
-      biceps: 0,
-      calves: 0,
-      chest: 0,
-      forearms: 0,
-      glutes: 0,
-      hamstrings: 0,
-      lowerback: 0,
-      midback: 0,
-      quadriceps: 0,
-      shoulders: 0,
-      obliques: 0,
-      triceps: 0,
-      upperback: 0,
-    },
-  };
+	state = {
+		isPrivate: true,
+		exercises: [],
+		checked: [],
+		loggedInUser: {},
+		appUserAccount: {},
+		muscleVals: {
+			abdominals: 0,
+			biceps: 0,
+			calves: 0,
+			chest: 0,
+			forearms: 0,
+			glutes: 0,
+			hamstrings: 0,
+			lowerback: 0,
+			midback: 0,
+			quadriceps: 0,
+			shoulders: 0,
+			obliques: 0,
+			triceps: 0,
+			upperback: 0
+		}
+	};
 
-  checkItem = exercise => {
-    const { checked } = this.state;
-    if (!checked.includes(exercise)) {
-      this.setState({ checked: [...checked, exercise] });
-    } else {
-      this.setState({ checked: checked.filter(a => a !== exercise) });
-    }
-  };
+	checkItem = (exercise) => {
+		const { checked } = this.state;
+		if (!checked.includes(exercise)) {
+			this.setState({ checked: [ ...checked, exercise ] });
+		} else {
+			this.setState({ checked: checked.filter((a) => a !== exercise) });
+		}
+	};
 
-  calculateMuscleVals = () => {
-    const { checked } = this.state;
-    const muscleVals = {
-      abdominals: 0,
-      biceps: 0,
-      calves: 0,
-      chest: 0,
-      forearms: 0,
-      glutes: 0,
-      hamstrings: 0,
-      lowerback: 0,
-      midback: 0,
-      quadriceps: 0,
-      shoulders: 0,
-      obliques: 0,
-      triceps: 0,
-      upperback: 0,
-    };
-    checked.forEach(exercise => {
-      muscleVals[exercise.major_muscle] += 3;
-      exercise.minor_muscles.forEach(muscle => {
-        muscleVals[muscle] += 1;
-      });
-    });
-    this.setState({ muscleVals });
-  };
+	calculateMuscleVals = () => {
+		const { checked } = this.state;
+		const muscleVals = {
+			abdominals: 0,
+			biceps: 0,
+			calves: 0,
+			chest: 0,
+			forearms: 0,
+			glutes: 0,
+			hamstrings: 0,
+			lowerback: 0,
+			midback: 0,
+			quadriceps: 0,
+			shoulders: 0,
+			obliques: 0,
+			triceps: 0,
+			upperback: 0
+		};
+		checked.forEach((exercise) => {
+			muscleVals[exercise.major_muscle] += 3;
+			exercise.minor_muscles.forEach((muscle) => {
+				muscleVals[muscle] += 1;
+			});
+		});
+		this.setState({ muscleVals });
+	};
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.checked !== this.state.checked) {
-      this.calculateMuscleVals();
-    }
-  }
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.checked !== this.state.checked) {
+			this.calculateMuscleVals();
+		}
+	}
 
-  render() {
+	render() {
+		return (
+			<View style={{ flex: 1 }}>
+				<View style={{ height: 350, marginTop: 10 }}>
+					<Model muscleVals={this.state.muscleVals} />
+				</View>
+				<Container>
+					<Content padder>
+						<View>
+							{this.state.exercises.map((exercise, i) => (
+								<ListItem
+									key={i}
+									title={exercise.title}
+									hideChevron
+									subtitle={`Major: ${exercise.major_muscle}`}
+									leftIcon={
+										<CheckBox
+											onPress={() => this.checkItem(exercise)}
+											checked={this.state.checked.includes(exercise)}
+										/>
+									}
+								/>
+							))}
+						</View>
+					</Content>
+				</Container>
+				<Button
+					title='Complete Workout'
+					onPress={() => {
+						this.completeWorkout;
+						this.props.navigation.navigate('CompletionModal');
+					}}
+				/>
+			</View>
+		);
+	}
 
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={{ height: 350, marginTop: 10 }}>
-          <Model muscleVals={this.state.muscleVals} />
-        </View>
-        <Container>
-          <Content padder>
-            <View>
-              {this.state.exercises.map((exercise, i) => (
-                <ListItem
-                  key={i}
-                  title={exercise.title}
-                  hideChevron
-                  subtitle={`Major: ${exercise.major_muscle}`}
-                  leftIcon={
-                    <CheckBox
-                      onPress={() => this.checkItem(exercise)}
-                      checked={this.state.checked.includes(exercise)}
-                    />
-                  }
-                />
-              ))}
-            </View>
-          </Content>
-        </Container>
-        <Button
-          title="Complete Workout"
-          onPress={() => this.props.navigation.navigate('CompletionModal')}
-        />
-        {/* <TouchableOpacity style={styles.completeWorkout} title='Complete Workout'>
-					<Text>Complete Workout</Text>
-				</TouchableOpacity> */}
-      </View>
-    );
-  }
+	componentDidMount() {
+		const currentWorkout = this.props.navigation.state.params.currentWorkout;
+		const appUserAccount = this.props.navigation.state.params.appUserAccount;
+		this.setState({ exercises: currentWorkout, appUserAccount });
+	}
 
-  componentDidMount() {
-    const currentWorkout = this.props.navigation.state.params.currentWorkout;
-    this.setState({ exercises: currentWorkout });
-  }
+	completeWorkout = () => {
+		this.saveWorkout();
+		const username = this.state.appUserAccount.user_name;
+		const nameWorkout = `${this.state.appUserAccount.user_name}s_workout${this.state.appUserAccount._id}`;
+		console.log(nameWorkout, username);
+		axios
+			.post(`${URL}/workouts/${nameWorkout}`, {
+				completed_by: username
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	saveWorkout = () => {
+		this.postWorkout();
+		const nameWorkout = `${this.state.appUserAccount.user_name}s_workout${this.state.appUserAccount._id}`;
+		const username = this.state.appUserAccount.user_name;
+		console.log(nameWorkout, username);
+		axios.post(`${URL}/workouts/${nameWorkout}/save/${username}`).catch((err) => {
+			console.log(err);
+		});
+	};
+
+	postWorkout = () => {
+		const nameWorkout = `${this.state.appUserAccount.user_name}s_workout${this.state.appUserAccount._id}`;
+		const username = this.state.appUserAccount.user_name;
+		const isPrivate = this.state.isPrivate;
+		const exercises = this.state.exercises.map((exercise) => exercise.title);
+		console.log(nameWorkout, username, isPrivate, exercises);
+		axios
+			.post(`${URL}/workouts`, {
+				name: nameWorkout,
+				exercises: exercises,
+				private: isPrivate,
+				created_by: username
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 }
 const styles = StyleSheet.create({
-  completeWorkout: {
-    padding: 10,
-    margin: 10,
-    fontSize: 24,
-    backgroundColor: 'powderblue',
-  },
+	completeWorkout: {
+		padding: 10,
+		margin: 10,
+		fontSize: 24,
+		backgroundColor: 'powderblue'
+	}
 });
--- workout-planner-fe/views/WorkoutPreview.js
+++ workout-planner-fe/views/WorkoutPreview.js
@@ -1,51 +1,50 @@
 import React from 'react';
-import {
-  StyleSheet, Text, View, Button, TouchableOpacity,
-} from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import WorkoutPreviewList from '../components/WorkoutPreviewList';
 
 export default class WorkoutPreview extends React.Component {
-  render() {
-    const { params } = this.props.navigation.state;
-    const currentWorkout = params.currentWorkout;
-    const appUserAccount = params.appUserAccount;
-    const currentUser = params.currentUser;
-    return (
-      <View style={styles.outerContainer}>
-        <WorkoutPreviewList
-          currentWorkout={currentWorkout}
-          currentUser={currentUser}
-          appUserAccount={appUserAccount}
-          navigation={this.props.navigation}
-        />
-        <TouchableOpacity
-          style={styles.linkContainer}
-          onPress={() => this.props.navigation.navigate('CompanionScreen', { currentWorkout })}
-        >
-          <Text style={styles.linkText}>Start Workout</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+	render() {
+		const { params } = this.props.navigation.state;
+		const currentWorkout = params.currentWorkout;
+		const appUserAccount = params.appUserAccount;
+		const currentUser = params.currentUser;
+		return (
+			<View style={styles.outerContainer}>
+				<WorkoutPreviewList
+					currentWorkout={currentWorkout}
+					currentUser={currentUser}
+					appUserAccount={appUserAccount}
+					navigation={this.props.navigation}
+				/>
+				<TouchableOpacity
+					style={styles.linkContainer}
+					onPress={() =>
+						this.props.navigation.navigate('CompanionScreen', { currentWorkout, appUserAccount })}
+				>
+					<Text style={styles.linkText}>Start Workout</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  outerContainer: {
-    margin: 10,
-    flex: 1,
-  },
-  linkContainer: {
-    margin: 5,
-    padding: 10,
-    backgroundColor: 'rgba(44,73,127, 0.7)',
-    borderColor: 'rgba(44,73,127, 1)',
-    borderWidth: 2,
-    borderRadius: 4,
-  },
-  linkText: {
-    fontSize: 20,
-    fontFamily: 'Roboto-Medium',
-    color: '#fff',
-    textAlign: 'center',
-  },
+	outerContainer: {
+		margin: 10,
+		flex: 1
+	},
+	linkContainer: {
+		margin: 5,
+		padding: 10,
+		backgroundColor: 'rgba(44,73,127, 0.7)',
+		borderColor: 'rgba(44,73,127, 1)',
+		borderWidth: 2,
+		borderRadius: 4
+	},
+	linkText: {
+		fontSize: 20,
+		fontFamily: 'Roboto-Medium',
+		color: '#fff',
+		textAlign: 'center'
+	}
 });
